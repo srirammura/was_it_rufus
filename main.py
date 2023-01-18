ecadf41116e50ac04b9af01b8597e8c5401db968
Sriram Muralidharan <54003774+srirammura@users.noreply.github.com>
2023-01-18 00:29:31 -0500

    Added docstrings

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+import os
 import git
 import datetime
 import argparse
-import os
 
 
 class was_it_rufus:
+    """
+    A class that instantiates all variables and methods about git status.
 
+    ...
+
+    Methods
+    -------
+        Prints the git status
+    """
     def __init__(self, git_directory):
+        """
+        Constructs all the necessary attributes for the Rufus object.
 
+        Parameters
+        ----------
+            repo : git object
+                repo object to use for various functionalities
+            active_branch : boolean
+                true or false if the branch is active or not
+            local_changes : boolean
+                true or false if the changes were made local or not
+            recent_commit : boolean
+                true or false if there was a recent commit
+            blame_rufus : boolean
+                true or false if rufus was the author or not
+        """
         self.repo = git.Repo(git_directory)
 
         self.active_branch = self.repo.active_branch.name
@@ -17,6 +40,13 @@ class was_it_rufus:
         self.blame_rufus = self.repo.head.commit.author.name == "Rufus"
 
     def git_status(self):
+        """
+        Prints the details of git status.
+
+        Returns
+        -------
+        None
+        """
         print("active branch: ", self.active_branch)
         print("local changes: ", self.local_changes)
         print("recent commit: ", self.recent_commit)
@@ -25,13 +55,12 @@ class was_it_rufus:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Was it rufus?')
-    parser.add_argument('git_directory', type=str,
-                        help='name of the git repo directory')
-
+    parser.add_argument('git_directory', type=str, help='name of the git repo directory')
     args = parser.parse_args()
 
+    #checks if the parsed argument is a directory or not
     if os.path.isdir(args.git_directory):
         rufus_obj = was_it_rufus(args.git_directory)
         rufus_obj.git_status()
     else:
-        print(args.git_directory, "Invalid directory.")
+        print(args.git_directory, "Invalid directory")
